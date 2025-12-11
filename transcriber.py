13d8ec115dd58b4c50922c5382589748e9f89518
Ibtihel Mhamdi <ibtihel.mhamdi@supcom.tn>
2025-12-11 15:08:00 +0100

    Initial commit

---
+++ transcriber.py
@@ -0,0 +1,22 @@
+import subprocess
+import whisper
+import os
+#subprocess executes shell commands
+def extract_audio(video_path: str, audio_path: str = "temp_audio.wav") -> str:
+    if os.path.exists(audio_path):
+        os.remove(audio_path)
+    command = [
+        "ffmpeg",
+        "-i",video_path,
+        "-q:a","0",
+        "-map","a",
+        audio_path,
+        "-y"
+    ]
+    subprocess.call(command,stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+    return audio_path
+def transcribe_audio(audio_path: str, model_size: str = "base") -> str:
+    model = whisper.load_model(model_size)
+    result = model.transcribe(audio_path)
+    transcript = result["text"]
+    return transcript
